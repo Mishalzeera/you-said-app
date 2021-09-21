48c723154e6ca1053da32b1843e5bb0d9be2f8d7
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-09-21 10:14:23 +0200

    Add templates and routes for individual pages

--- app.py
+++ app.py
@@ -9,6 +9,26 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/partner_one")
+def partner_one():
+    return render_template("partner_one.html")
+
+
+@app.route("/partner_two")
+def partner_two():
+    return render_template("partner_two.html")
+
+
+@app.route("/login_register")
+def login_register():
+    return render_template("login_register.html")
+
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
